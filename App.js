f028c750e3c9f2bcddf804ec4c0329553aa72325
miyesier <miyesier@gmail.com>
2019-11-06 15:15:47 +0100

    Styles homepage.

--- App.js
+++ App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react'
 import HomeScreen from "./Screens/Home/HomeScreen"
-import { Text } from 'react-native';
 
 export default class App extends Component {
   render(){
     return (
       <>
-        <Text>Hello from App</Text>
-        <HomeScreen />
+       <HomeScreen />
       </>
     )
   }
--- Screens/Home/HomeScreen.js
+++ Screens/Home/HomeScreen.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 import { GetArticles } from '../../Services/ArticlesApiService';
-import { Image } from "react-native-elements"
+import { Image, colors } from "react-native-elements"
+import { bold } from 'ansi-colors';
 
 export default class HomeScreen extends Component {
   state = { 
@@ -20,19 +21,23 @@ export default class HomeScreen extends Component {
     return (
       <View>
         <Image 
-          style={{ widht: 100, height: 100 }}
+          style={styles.image}
           source={{ uri: article.image }}
         />
-        <Text>{article.title}</Text>    
-        <Text>{article.content}</Text> 
+        <Text style ={styles.title} >
+          {article.title}
+        </Text>    
+        
+        <Text style={styles.content}>
+          {article.content}</Text> 
       </View>
     )
   }
 
   render() {
     return (
-      <View style={StyleSheet.container}>
-        <Text>Hello from Homescreen</Text>
+      <View style={styles.container}>
+        <Text style={styles.header}>Classy News</Text>
         <FlatList 
           data={this.state.articles}
           renderItem={this.renderArticles}
@@ -44,10 +49,36 @@ export default class HomeScreen extends Component {
 }
 
 const styles = StyleSheet.create({
+  header: {
+    margin: 30,
+    fontSize: 40,
+    fontFamily: 'Cochin',
+    fontWeight: 'bold'
+  },
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "white",
     alignItems: "center",
     justifyContent: "center"
+
+  },
+  image:{
+   width: 130, 
+   height: 100,
+   marginLeft: 20,
+   padding:90
+  },
+  title: {
+    padding: 10,
+    fontSize: 30,
+    fontFamily: 'Cochin',
+    fontWeight: 'bold'
+  },
+  content:{
+    margin:1,
+    padding: 10,
+    fontSize: 25,
+    fontFamily: 'Cochin',
+    color: '#363737'
   }
 });
