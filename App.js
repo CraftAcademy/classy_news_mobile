657324b11dbed89ba6490f27f2cd6f09e161cab6
Bulut <bulut@buluts-mbp.mynet>
2019-11-07 13:53:43 +0100

    importes AppNavigator in app.js and adds button.

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import HomeScreen from "./Screens/Home/HomeScreen"
+import AppNavigator from './AppNavigator'
 
 export default class App extends Component {
   render(){
     return (
       <>
        <HomeScreen />
+       <AppNavigator />
       </>
     )
   }
--- Screens/Article/ArticleScreen.js
+++ Screens/Article/ArticleScreen.js
@@ -2,10 +2,22 @@ import React, { Component } from 'react'
 import { Text, View } from 'react-native'
 
 export default class ArticleScreen extends Component {
-  render() {
+  renderArticles({item}) {
+  const article = item
   return (
     <View>
     <Text>Hello World from Article Scren!!!</Text>
+    <Image
+        style={{ width: 100, height: 100 }}
+        source={{ uri: article.image }}
+      />
+      <Text>{article.category.name}</Text>
+      <Text>{article.title}</Text>
+      <Text>{article.description}</Text>
+      <Button
+        title="View Article"
+        onPress={() => this.showArticle()}
+      />
     </View>
   )
 }
