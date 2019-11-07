366b9de593019e0c83e0c6a64fbf2a3a0b8a276b
Yasmine Ezequiel <yasmine.ezequiel@gmail.com>
2019-11-07 13:25:10 +0100

    Installs react navigation and creates Article Screen.

---
+++ Screens/Article/ArticleScreen.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export default class ArticleScreen extends Component {
+  render() {
+  return (
+    <View>
+    <Text>Hello World from Article Scren!!!</Text>
+    </View>
+  )
+}
+}
